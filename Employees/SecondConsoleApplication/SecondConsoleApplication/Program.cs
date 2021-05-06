f0a9e0a1d7f77e99c0084aebea4a7f4d53eb44fa
GulIrina86 <83182298+GulIrina86@users.noreply.github.com>
2021-05-06 18:36:40 +0300

    д\з 2

---
+++ Employees/SecondConsoleApplication/SecondConsoleApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+namespace SecondConsoleApplication
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string path = @"E:\projects\Employees.csv";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string input = null; 
+                while ((input = sr.ReadLine()) != null)
+                {
+
+                    Console.WriteLine(input);
+                }
+            }
+        }
+    }
+}
